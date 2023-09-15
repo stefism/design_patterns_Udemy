6c1d028b13a44acb04bf54a0905fe8ac4dd8fb08
Stefan Markov <stefan.t.markov@gmail.com>
2023-09-15 17:31:55 +0300

    Add code

--- Solid/Program.cs
+++ Solid/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! test test");
+﻿using System.Diagnostics;
+
+namespace DesignPatterns
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; //memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries);
+        }
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal journal, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, journal.ToString());
+            }
+        }
+    }
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var journal = new Journal();
+            journal.AddEntry("Cool name 1");
+            journal.AddEntry("Name cool 2");
+            Console.WriteLine(journal);
+
+            var persitence = new Persistence();
+            var filename = @"C:\PROJECTS\design_patterns_Udemy\Solid\journal.txt";
+
+            persitence.SaveToFile(journal, filename, true);
+            //Process.Start(filename); //not work for me.
+        }
+    }
+}
