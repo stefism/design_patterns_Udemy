bbb9a33f48a2d0bc49cb53d983209fe33a1907c2
Stefan Markov <stef4otm@gmail.com>
2023-09-27 14:43:28 +0300

    270923

---
+++ Builder/Builder/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+
+namespace DesignPatterns
+{
+    public class HtmlElement
+    {
+        public string Name, Text;
+        public List<HtmlElement> Elements = new List<HtmlElement>();
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+            
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize + indent);
+            sb.AppendLine($"{i}<{Name}>");
+
+            if(!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var e in Elements)
+            {
+                sb.Append(e.ToStringImpl(indent + 1));
+            }
+
+            sb.AppendLine($"{i}</{Name}>");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+
+    public class HtmlBuilder
+    {
+        private readonly string rootName;
+        HtmlElement root = new HtmlElement();
+
+        public HtmlBuilder(string rootName)
+        {
+            this.rootName = rootName;
+            root.Name = rootName;
+        }
+
+        public HtmlBuilder AddChild(string childName, string childText)
+        {
+            var htmlElement = new HtmlElement(childName, childText);
+            root.Elements.Add(htmlElement);
+            return this; //Така правим флуент интерфейс. Можем много пъти да пишем един след друг .AddChild
+        }
+
+        public void Clear()
+        {
+            root = new HtmlElement { Name = rootName };
+        }
+
+        public override string ToString()
+        {
+            return root.ToString();
+        }
+    }
+
+    public class Builder
+    {
+        static void Main(string[] args)
+        {
+            var htmlBuilder = new HtmlBuilder("ul");
+            htmlBuilder.AddChild("li", "Hello").AddChild("li", "world."); // флуент интерфейс.
+            Console.WriteLine(htmlBuilder.ToString());
+        }
+    }
+}
