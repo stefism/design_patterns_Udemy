30c87512de264628455e80c297f57d18e5e47ddf
Stefan Markov <stef4otm@gmail.com>
2023-10-03 12:29:31 +0300

    031023

---
+++ Builder/FunctionalBuider2/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace DesignPattern
+{
+    public class Person
+    {
+        public string Name, Position;
+    }
+
+    public abstract class FunctionalBuilder<TSubject, TSelf>
+        where TSelf : FunctionalBuilder<TSubject, TSelf>
+        where TSubject : new()
+    {
+        private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
+
+        public TSelf Do(Action<Person> action) => AddAction(action);
+
+        public Person Build() => actions.Aggregate(new Person(), (person, function) => function(person));
+
+        private TSelf AddAction(Action<Person> action)
+        {
+            actions.Add(person =>
+            {
+                action(person);
+                return person;
+            });
+
+            return (TSelf)this;
+        }
+    }
+
+    public sealed class PersonBuilder : FunctionalBuilder<Person, PersonBuilder>
+    {
+        public PersonBuilder Called(string name) => Do(person => person.Name = name);
+    }
+
+    public static class PersonBuilderExtensions
+    {
+        public static PersonBuilder WorksAs(this PersonBuilder builder, string position)
+            => builder.Do(person => person.Position = position);
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new PersonBuilder().Called("Sarah").WorksAs("Developer").Build();
+            ;
+        }
+    }
+}
---
+++ Builder/FunctionalBuilder/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DesignPattern
+{
+    public class Person
+    {
+        public string Name, Position;
+    }
+
+    public sealed class PersonBuilder
+    {
+        private readonly List<Func<Person, Person>> actions = new List<Func<Person, Person>>();
+
+        public PersonBuilder Called(string name) => Do(person => person.Name = name);
+
+        public PersonBuilder Do(Action<Person> action) => AddAction(action);
+
+        public Person Build() => actions.Aggregate(new Person(), (person, function) => function(person));
+
+        private PersonBuilder AddAction(Action<Person> action)
+        {
+            actions.Add(person =>
+            {
+                action(person);
+                return person;
+            });
+
+            return this;
+        } 
+    }
+
+    public static class PersonBuilderExtensions
+    {
+        public static PersonBuilder WorksAs(this PersonBuilder builder, string position)
+            => builder.Do(person => person.Position = position);
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var person = new PersonBuilder().Called("Sarah").WorksAs("Developer").Build();
+            ;
+        }
+    }
+}
