0bcd12e1e81e87671de72c43eb57257e28de0d2b
Stefan Markov <stef4otm@gmail.com>
2023-10-03 18:01:14 +0300

    0310223

---
+++ Builder/FacetedBuilder/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace DesignPatterns
+{
+    public class Person
+    {
+        // address
+        public string StreetAddress, Postcode, City;
+
+        // employment
+        public string CompanyName, Position;
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{StreetAddress}, {Postcode}, {City}";
+        }
+    }
+
+    public class PersonBuilder // facade
+    {
+        // reference
+        protected Person person = new Person();
+
+        public PersonJobBuilder Works => new PersonJobBuilder(person);
+    }
+
+    public class PersonAddressBuilder : PersonBuilder
+    { 
+
+    }
+
+    public class PersonJobBuilder : PersonBuilder
+    {
+        public PersonJobBuilder(Person person)
+        {
+            this.person = person;
+        }
+
+        public PersonJobBuilder At(string companyName)
+        {
+            person.CompanyName = companyName;
+            return this;
+        }
+
+        public PersonJobBuilder AsA(string position)
+        {
+            person.Position = position;
+            return this;
+        }
+
+        public PersonJobBuilder Earning(int amount)
+        {
+            person.AnnualIncome = amount;
+            return this;
+        }
+    }
+
+
+    public class Demo
+    {
+        static void Main(string[] args)
+        {
+            var pb = new PersonBuilder();
+            var person = pb.Works.At("Money").AsA("Engineer").Earning(123000);
+        }
+    }
+}
