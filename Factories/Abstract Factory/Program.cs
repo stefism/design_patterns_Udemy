530924cdd00e1f993b9095a661f61226a8664271
Stefan Markov <stef4otm@gmail.com>
2023-10-06 10:33:25 +0300

    061023

--- Factories/Abstract Factory/Program.cs
+++ Factories/Abstract Factory/Program.cs
@@ -1,12 +1,69 @@
 ﻿namespace DesignPatterns
 {
+    public interface IHotDrink
+    {
+        void Consume();
+    }
 
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Tea tea tea");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Coffee coffee coffee");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Prepare {amount} Tea");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Prepare {amount} Coffee");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink
+        {
+            Coffee, Tea
+        }
+
+        private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new Dictionary<AvailableDrink, IHotDrinkFactory>();
+
+        public HotDrinkMachine()
+        {
+            //foreach (AvailableDrink dring in Enum.GetValues)
+        }
+    }
 
     public class Demo
     {
         public static void Main(string[] args)
         {
-           
+
         }
     }
 }
